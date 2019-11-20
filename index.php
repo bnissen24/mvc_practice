795472bd4cd0e79e76b1e64d78026a06c9317870
Bryan Nissen <bryannissen@gmail.com>
2019-11-20 14:40:07 -0500

    Initial MVC Project

---
+++ index.php
@@ -0,0 +1,25 @@
+ines (49 sloc)  1.44 KB
+
+<?php
+require('model/database.php');
+require('model/accounts_db.php');
+require('model/questions_db.php');
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'show_login';
+    }
+}
+
+switch ($action) {
+    case 'show_login': {
+        include('views/login.php');
+        break;
+    }
+
+    default: {
+        $error = 'Unknown Action';
+        include('errors/error.php');
+    }
+}
